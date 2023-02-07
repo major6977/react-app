2298819a92a28da4c9d35651d6732538d6f177f8
major6977 <majordav@msn.com>
2023-02-06 19:58:07 -0700

    react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const age = 17;
+  const isGreen = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <h1 style={{ color: isGreen ? "green" : "red" }}> This has color</h1>
+      {isGreen && <button>This is a button</button>}
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Job salary={120000} position="senior sde" company="IBM" />
+//       <Job salary={80000} position="doorman" company="Trump Tower" />
+//       <Job salary={150000} position="Developer" company="Intel" />
+//     </div>
+//   );
+// }
+
+// const Job = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.salary}</h1>
+//       <h1>{props.position}</h1>
+//       <h1>{props.company}</h1>
+//     </div>
+//   );
+// };
 
 export default App;
