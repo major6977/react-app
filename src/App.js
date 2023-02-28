fa30c9f2bb29a795c8cd0329eb97028a1c1305ca
major6977 <majordav@msn.com>
2023-02-28 16:32:52 -0700

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import { Navbar } from "./Navbar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { useState } from "react";
+import { Form } from "./components/Form";
 
 function App() {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: true,
-      },
-    },
-  });
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1> PAGE NOT FOUND </h1>} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
 
 export default App;
+
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Home } from "./pages/Home";
+// import { Profile } from "./pages/Profile";
+// import { Contact } from "./pages/Contact";
+// import { Navbar } from "./Navbar";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { useState } from "react";
+
+// const client = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       refetchOnWindowFocus: true,
+//     },
+// },
+// });
+
+// <div className="App">
+//   /* <QueryClientProvider client={client}>
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="*" element={<h1> PAGE NOT FOUND </h1>} />
+//       </Routes>
+//     </Router>
+//   // </QueryClientProvider/
+// </div>
+//   );
+// }
 
 // import Axios from "axios";
 // import { useEffect, useState } from "react";
