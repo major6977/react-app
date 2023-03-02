4f8842e67916382b7dfb9899bf57602891ac3a69
major6977 <majordav@msn.com>
2023-03-02 10:16:25 -0700

    creating custome hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-import { Form } from "./components/Form";
+import { useToggle } from "./useToggle";
+// import { Form } from "./components/Form";
 
 function App() {
+  const [isVisible, toggle] = useToggle();
+  const [isVisible2, toggle2] = useToggle();
+
   return (
     <div className="App">
-      <Form />
+      <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
+      {isVisible ? "Hide" : "Show"}
+      {isVisible && <h1> Hidden Text</h1>}
+
+      <button onClick={toggle2}>{isVisible2 ? "Hide" : "Show"}</button>
+      {isVisible ? "Hide" : "Show"}
+      {isVisible2 && <h1> Hidden Text</h1>}
     </div>
   );
 }
